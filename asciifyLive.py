8d7c12c928094eaad8c7df8f77f66fce5a137ac1
baton96 <podolce0@gmail.com>
2020-08-21 10:01:50 +0200

    Cleaning code

--- asciifyLive.py
+++ asciifyLive.py
@@ -1,29 +1,27 @@
-def show_webcam():
-    from cv2 import flip, resize, VideoCapture
-    from numpy import asarray, sum
-    from os import system
-    chars = asarray(list(' .,:;irsXA253hMHGS#9B&@'))
-    cam = VideoCapture(0)
-    try:
-        while True:
-            img = cam.read()[1]
-            img = sum(
-                flip(resize(
-                    img, (120, 64),
-                    interpolation=0
-                ), 0), axis=2
-            )
-            mi, ma = img.min(), img.max()
-            img = (
-                    ((img - mi) / (ma - mi)) * 22
-            ).astype(int)
-            toPrint = '\n'.join(
-                [''.join(r) for r in chars[img]]
-            )
-            system('cls')
-            print(toPrint)
-    except KeyboardInterrupt:
-        cam.release()
+from os import system
 
+from cv2 import flip, resize, VideoCapture
+from numpy import asarray, sum
 
-show_webcam()
+chars = asarray(list(' .,:;irsXA253hMHGS#9B&@'))
+cam = VideoCapture(0)
+try:
+    while True:
+        img = cam.read()[1]
+        img = sum(
+            flip(resize(
+                img, (120, 64),
+                interpolation=0
+            ), 0), axis=2
+        )
+        mi, ma = img.min(), img.max()
+        img = (
+                ((img - mi) / (ma - mi)) * 22
+        ).astype(int)
+        toPrint = '\n'.join(
+            [''.join(r) for r in chars[img]]
+        )
+        system('cls')
+        print(toPrint)
+except KeyboardInterrupt:
+    cam.release()
